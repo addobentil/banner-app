db54e734c7404befc871fdd66a2d0510e3bd4bab
Your Name <addobentil100@gmail.com>
2025-03-18 02:58:48 +0000

    Updates

---
+++ precache-manifest.1434dceee5e70340822e933899d15807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5ffc000c6323e62eeccb9153631404",
+    "url": "/banner-app/index.html"
+  },
+  {
+    "revision": "acbaf8b738a2201d78f0",
+    "url": "/banner-app/static/css/main.481dc228.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/banner-app/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "acbaf8b738a2201d78f0",
+    "url": "/banner-app/static/js/main.0ce4647d.chunk.js"
+  },
+  {
+    "revision": "bd31ae7501402eff3a30",
+    "url": "/banner-app/static/js/runtime~main.c935319d.js"
+  }
+]);
