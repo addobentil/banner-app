7b5380208f9c109bae74cf016cdcb9ded7ac2721
Your Name <addobentil100@gmail.com>
2025-03-18 00:51:26 +0000

    modified control form

--- src/components/controlForm.js
+++ src/components/controlForm.js
@@ -14,7 +14,7 @@ const ControlForm = () => {
         <h2 style={{marginBottom: '25px'}}>Interactive Banner Design</h2>
 
         {/* Background Color */}
-        <div className="form-gro">
+        <div className="form-group">
           <label>Background Color:</label>
           <input style={{height: '50px', cursor: 'pointer'}}
             type="color"
